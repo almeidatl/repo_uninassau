7486f1a9a067dafef87f09a11c5def56c14c03c0
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2025-03-25 22:29:30 -0300

    ini spring boot

---
+++ backend_frameworks/ReservaCoworking/src/main/java/dev/almeida/ReservaCoworking/ReservaCoworkingApplication.java
@@ -0,0 +1,13 @@
+package dev.almeida.ReservaCoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaCoworkingApplication.class, args);
+	}
+
+}
---
+++ backend_frameworks/ReservaCoworking/src/test/java/dev/almeida/ReservaCoworking/ReservaCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.almeida.ReservaCoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
