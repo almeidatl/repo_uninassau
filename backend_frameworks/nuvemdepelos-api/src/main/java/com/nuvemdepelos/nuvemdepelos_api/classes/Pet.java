3567714f559c08f0f0afd831300b63813f2345c2
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2025-05-25 09:52:30 -0300

    Update Pet.java

--- backend_frameworks/nuvemdepelos-api/src/main/java/com/nuvemdepelos/nuvemdepelos_api/classes/Pet.java
+++ backend_frameworks/nuvemdepelos-api/src/main/java/com/nuvemdepelos/nuvemdepelos_api/classes/Pet.java
@@ -1,16 +1,26 @@
 package com.nuvemdepelos.nuvemdepelos_api.classes;
 
 public class Pet {
+    private int id;
     private String nome;
     private String raca;
     private int idade;
     private String sexo;
 
-    public Pet(String name, String raca, int idade, String sexo) {
+    public Pet(int id, String name, String raca, int idade, String sexo) {
+        this.id = id;
         this.nome = name;
         this.raca = raca;
         this.idade = idade;
         this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
