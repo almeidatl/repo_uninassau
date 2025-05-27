c10109f8158979f7c17b020e90665bbf030dfb6a
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2025-05-26 23:03:12 -0300

    att services

--- backend_frameworks/AgendaMedica/src/main/java/com/clinicare/AgendaMedica/service/ConsultaService.java
+++ backend_frameworks/AgendaMedica/src/main/java/com/clinicare/AgendaMedica/service/ConsultaService.java
@@ -1,4 +1,22 @@
 package com.clinicare.AgendaMedica.service;
 
+import com.clinicare.AgendaMedica.entity.Consulta;
+import com.clinicare.AgendaMedica.repository.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ConsultaService {
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public List<Consulta> listarConsultas() {
+        return consultaRepository.findAll();
+    }
+
+    public Consulta salvarConsulta(Consulta consulta) {
+        return consultaRepository.save(consulta);
+    }
 }
--- backend_frameworks/AgendaMedica/src/main/java/com/clinicare/AgendaMedica/service/PacienteService.java
+++ backend_frameworks/AgendaMedica/src/main/java/com/clinicare/AgendaMedica/service/PacienteService.java
@@ -1,4 +1,23 @@
 package com.clinicare.AgendaMedica.service;
 
+import com.clinicare.AgendaMedica.entity.Paciente;
+import com.clinicare.AgendaMedica.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PacienteService {
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public List<Paciente> listarPacientes() {
+        return pacienteRepository.findAll();
+    }
+
+    public Paciente salvarPaciente(Paciente paciente) {
+        return pacienteRepository.save(paciente);
+    }
+
 }
